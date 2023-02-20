b4276efe3d3eee0b6355bd34fa514a9fe8a02868
Leonardoozx <leonardoguimaraesurci@gmail.com>
2023-02-20 20:14:08 -0300

    chore: adds `imgur` remotePattern

--- next.config.js
+++ next.config.js
@@ -1,6 +1,18 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+  images: {
+      remotePatterns: [
+      // path for imgur images, now using for LinkedIn and GitHub .png logo
+      // https://imgur.com/{imageId}
+      {
+        protocol: 'https',
+        hostname: 'imgur.com',
+        port: '',
+        pathname: '/*.png'
+      }
+    ],
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
