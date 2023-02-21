ee19279e0fd509dca945e8548a66e41214933cd8
Leonardoozx <leonardoguimaraesurci@gmail.com>
2023-02-20 21:04:43 -0300

    feat: adds linkedin and github images

--- next.config.js
+++ next.config.js
@@ -2,15 +2,15 @@
 const nextConfig = {
   reactStrictMode: true,
   images: {
-      remotePatterns: [
+    remotePatterns: [
       // path for imgur images, now using for LinkedIn and GitHub .png logo
       // https://imgur.com/{imageId}
       {
         protocol: 'https',
         hostname: 'imgur.com',
         port: '',
-        pathname: '/*.png'
-      }
+        pathname: '/*.png',
+      },
     ],
   },
 };
--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,4 +1,4 @@
-// import Image from "next/image";
+import Image from "next/image";
 import { StyledHeader, StyledImagesContainer, StyledNav } from "./styled";
 
 export default function Header() {
@@ -6,14 +6,17 @@ export default function Header() {
     <StyledHeader>
       <h2>Leonardo G. Urci</h2>
       <StyledNav>
+        <div>
         <a className="header-links" href="#">{"<Home />"}</a>
         <a className="header-links" href="#">{"<About />"}</a>
         <a className="header-links" href="#">{"<Projects />"}</a>
+        </div>
+        <StyledImagesContainer>
+          <Image src="https://imgur.com/sHEHc7v.png" alt="LinkedIn" width={25} height={25} />
+          <Image src="https://imgur.com/ZsNt1NJ.png" alt="GitHub" width={25} height={25} />
+        </StyledImagesContainer>
       </StyledNav>
-      <StyledImagesContainer>
-          {/* <Image src="##" alt="LinkedIn" width={500} height={500} /> */}
-          {/* <Image src="##" alt="GitHub" width={500} height={500} /> */}
-      </StyledImagesContainer>
+
     </StyledHeader>
   );
 }
