0dfe6457b824083b8bd2a9ea7fce0032088088b9
Leonardoozx <leonardoguimaraesurci@gmail.com>
2023-02-21 20:46:35 -0300

    feat: implements `Header` component

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,22 +1,31 @@
 import Image from "next/image";
-import { StyledHeader, StyledImagesContainer, StyledNav } from "./styled";
+import { StyledHeader, StyledImagesContainer, StyledNav, StyledNavLinks } from "./styled";
 
 export default function Header() {
   return (
     <StyledHeader>
-      <h2>Leonardo G. Urci</h2>
+      <h2 id="name">Leonardo G. Urci</h2>
       <StyledNav>
-        <div>
-        <a className="header-links" href="#">{"<Home />"}</a>
-        <a className="header-links" href="#">{"<About />"}</a>
-        <a className="header-links" href="#">{"<Projects />"}</a>
-        </div>
+
+        {/* all links to other page paths, such as '/', '/about' and '/projects' */}
+        <StyledNavLinks>
+          <a className="header-links" href="#">{"<Home />"}</a>
+          <a className="header-links" href="#">{"<About />"}</a>
+          <a className="header-links" href="#">{"<Projects />"}</a>
+        </StyledNavLinks>
+
+        {/* all links to other social medias */}
         <StyledImagesContainer>
-          <Image src="https://imgur.com/sHEHc7v.png" alt="LinkedIn" width={25} height={25} />
-          <Image src="https://imgur.com/ZsNt1NJ.png" alt="GitHub" width={25} height={25} />
+          {/* LinkedIn link */}
+          <a href="https://linkedin.com/in/leonardoozx" target="_blank" rel="noopener noreferrer">
+            <Image src="https://imgur.com/KHYYe8Q.png" alt="LinkedIn" width={25} height={25} className="social-media-img" />
+          </a>
+          {/* GitHub link */}
+          <a href="https://github.com/Leonardoozx" target="_blank" rel="noopener noreferrer">
+            <Image src="https://imgur.com/TY8NF0x.png" alt="GitHub" width={25} height={25} className="social-media-img" />
+          </a>
         </StyledImagesContainer>
       </StyledNav>
-
     </StyledHeader>
   );
 }
