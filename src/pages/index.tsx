6e88fe5cb28c5b69c9ee9ac0bbc88b8d2c352e5c
Leonardoozx <leonardoguimaraesurci@gmail.com>
2023-02-21 18:57:04 -0300

    style: adds theme colors for header and the primary one

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,18 +3,22 @@ import { ThemeProvider } from 'styled-components';
 import TypeWriter from 'typewriter-effect';
 
 const theme = {
-  mainColor: 'black'
+  colorPalette: {
+    headerColor: '#212336',
+    // backgroundColor: #23263a, default setted on GlobalStyles file
+    primaryColor: '#ff4a57',
+  }
 }
 
 export default function Home() {
   return (
     <ThemeProvider theme={theme}>
-    <Header />
-    <TypeWriter
-      onInit={(typewriter) => {
+      <Header />
+      <TypeWriter
+        onInit={(typewriter) => {
         typewriter.typeString('leonardo').start()
-      }}
-    />
+        }}
+      />
     </ThemeProvider>
   )
 }
