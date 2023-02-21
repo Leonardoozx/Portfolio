aa23cbb3ce8065928b08383c721a4e37f8f4b34c
Leonardoozx <leonardoguimaraesurci@gmail.com>
2023-02-21 20:38:12 -0300

    style: implements header mobile and desktop compability

--- src/components/Header/styled.ts
+++ src/components/Header/styled.ts
@@ -1,28 +1,98 @@
-import styled from "styled-components";
+import styled from 'styled-components';
 
 export const StyledHeader = styled.header`
   display: flex;
   align-items: center;
-  justify-content: space-between;
-  padding: 0 15px;
-  height: 65px;
-  border: 1px solid red;
+  justify-content: space-around;
+  flex-direction: column;
+  height: 170px;
+  background-color: ${({ theme }) => theme.colorPalette.headerColor};
+
+  @media screen and (min-width: 700px) {
+    flex-direction: row;
+    padding: 10px 10%;
+    justify-content: space-between;
+    height: 52%;
+  }
+
+  @media screen and (min-width: 900px) {
+    #name {
+      font-size: 28px;
+    }
+  }
+
+  @media screen and (min-width: 1600px) {
+    padding: 10px 17%;
+  }
 `;
 
 export const StyledNav = styled.nav`
   display: flex;
-  height: 100%;
+  justify-content: flex-end;
   flex-direction: column;
-  border: 1px solid black;
+  width: 100%;
+  height: 100px;
+  gap: 10px;
+
+  @media screen and (min-width: 700px) {
+    width: 55%;
+    flex-direction: row;
+  }
+`;
+
+export const StyledNavLinks = styled.div`
+  display: flex;
   align-items: center;
+  flex-direction: column;
   justify-content: space-around;
-  
-  .header-links {
-    letter-spacing: 0.5px;
+  height: 100%;
+  width: 100%;
+  gap: 5px;
+
+  @media screen and (min-width: 500px) {
+    flex-direction: row;
+  }
+
+  @media screen and (min-width: 900px) {
+    .header-links {
+      font-size: 18px;
+      padding: 0 10px;
+    }
+  }
+
+  @media screen and (min-width: 1000px) {
+    justify-content: flex-end;
+  }
+
+  @media screen and (min-width: 1300px) {
+    .header-links {
+      font-size: 22.5px;
+    }
+    
   }
 `;
 
 export const StyledImagesContainer = styled.div`
   display: flex;
-  flex-direction: column;
+  margin: 0 auto;
+  gap: 10px;
+  width: 30%;
+  justify-content: space-around;
+
+  @media screen and (min-width: 700px) {
+    width: fit-content;
+    flex-direction: column;
+    height: 100%;
+    justify-content: center;
+  }
+
+  @media screen and (min-width: 1000px) {
+    flex-direction: row;
+    align-items: center;
+
+    .social-media-img {
+      width: 30px;
+      height: 30px;
+    }
+  }
 `;
