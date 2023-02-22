5fa9d8fa31e37fda6b4040bec8be0dedc194e4f5
Leonardoozx <leonardoguimaraesurci@gmail.com>
2023-02-22 19:18:34 -0300

    feat: AuthProvider component

---
+++ src/context/AuthProvider.tsx
@@ -0,0 +1,20 @@
+import { useState } from 'react';
+import AuthContext from '.';
+import { IAuthContext, IAuthProvider } from './interface';
+
+export default function AuthProvider({ children }: IAuthProvider) {
+  // open and close the header burguer menu
+  const [isBurguerMenuOpen, setIsBurguerMenuOpen] = useState(false);
+
+  const setBurguerMenuStatus = (): void =>
+    setIsBurguerMenuOpen(!isBurguerMenuOpen);
+
+  const value: IAuthContext = {
+    burguerMenu: {
+      isOpen: isBurguerMenuOpen,
+      setBurguerMenuStatus,
+    },
+  };
+
+  return <AuthContext.Provider value={value}>{children}</AuthContext.Provider>;
+}
