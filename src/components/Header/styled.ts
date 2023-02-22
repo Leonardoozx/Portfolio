11dcc979b65ce9b72f64ada729705a7cbe7d8e82
Leonardoozx <leonardoguimaraesurci@gmail.com>
2023-02-22 20:41:54 -0300

    style: for burguer menu 'screen'

--- src/components/Header/styled.ts
+++ src/components/Header/styled.ts
@@ -4,15 +4,17 @@ export const StyledHeader = styled.header`
   display: flex;
   align-items: center;
   justify-content: space-around;
-  flex-direction: column;
-  height: 170px;
+  height: 60px;
+  padding: 10px 10%;
   background-color: ${({ theme }) => theme.colorPalette.headerColor};
+  position: relative;
+
+  #name {
+    width: 75%;
+  }
 
   @media screen and (min-width: 700px) {
-    flex-direction: row;
-    padding: 10px 10%;
     justify-content: space-between;
-    height: 52%;
   }
 
   @media screen and (min-width: 900px) {
@@ -27,15 +29,27 @@ export const StyledHeader = styled.header`
 `;
 
 export const StyledNav = styled.nav`
+  position: absolute;
   display: flex;
-  justify-content: flex-end;
+  gap: 25px;
   flex-direction: column;
-  width: 100%;
-  height: 100px;
-  gap: 10px;
+  justify-content: center;
+  right: 0;
+  top: 0;
+  width: 30vh;
+  height: 30vh;
+  background-color: ${({ theme }) => theme.colorPalette.backgroundColor};
+  border-radius: 0 0 0 30px;
+  box-shadow: 0px 2px 20px 2px rgba(1, 1, 1, 0.08);
 
-  @media screen and (min-width: 700px) {
+  @media screen and (min-width: 800px) {
+    gap: 10px;
+    position: relative;
+    background-color: transparent;
+    box-shadow: none;
+    display: flex;
     width: 55%;
+    height: 100px;
     flex-direction: row;
   }
 `;
@@ -43,17 +57,17 @@ export const StyledNav = styled.nav`
 export const StyledNavLinks = styled.div`
   display: flex;
   align-items: center;
-  flex-direction: column;
   justify-content: space-around;
-  height: 100%;
+  flex-direction: column;
   width: 100%;
+  height: 30%;
   gap: 5px;
 
-  @media screen and (min-width: 500px) {
-    flex-direction: row;
-  }
-
   @media screen and (min-width: 900px) {
+    height: 100%;
+    justify-content: space-around;
+    flex-direction: row;
+
     .header-links {
       font-size: 18px;
       padding: 0 10px;
@@ -66,29 +80,29 @@ export const StyledNavLinks = styled.div`
 
   @media screen and (min-width: 1300px) {
     .header-links {
-      font-size: 22.5px;
+      font-size: 20px;
     }
-    
   }
 `;
 
 export const StyledImagesContainer = styled.div`
   display: flex;
   margin: 0 auto;
-  gap: 10px;
   width: 30%;
   justify-content: space-around;
 
-  @media screen and (min-width: 700px) {
+  @media screen and (min-width: 800px) {
     width: fit-content;
     flex-direction: column;
     height: 100%;
     justify-content: center;
   }
-
+  
   @media screen and (min-width: 1000px) {
     flex-direction: row;
     align-items: center;
+    justify-content: space-around;
+    width: 90px;
 
     .social-media-img {
       width: 30px;
