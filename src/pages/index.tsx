de6b949bcf96b8bb039a4406e15d3f3777952899
Leonardoozx <leonardoguimaraesurci@gmail.com>
2023-02-22 19:21:29 -0300

    feat: welcome text

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,22 +1,21 @@
+import TypeWriterEffect from '@/components/TypeWriterEffect';
 import TypeWriter from 'typewriter-effect';
 
 export default function Home() {
   return (
-    <>
+    <div>
+      <TypeWriterEffect strings={"Hey, what's up?✌️"} />
       <TypeWriter
         onInit={(typewriter) => {
           typewriter
             .changeDelay(50)
-            .typeString("Hey, what's up?✌🏼")
-            .pauseFor(1000)
-            .deleteAll(10)
-            .typeString("Welcome to my portfolio!")
+            .typeString('Welcome to my portfolio!')
             .pauseFor(1000)
             .deleteAll(10)
             .typeString("I'm a FullStack Developer.")
-            .start()
+            .start();
         }}
       />
-    </>
-  )
+    </div>
+  );
 }
