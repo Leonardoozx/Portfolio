f0f808724fec44684814ce832acaa0e9a76889f7
Leonardoozx <leonardoguimaraesurci@gmail.com>
2023-02-24 19:56:47 -0300

    style: starts styling main message component

---
+++ src/components/MainMessage/styled.ts
@@ -0,0 +1,9 @@
+import styled from 'styled-components';
+
+export const StyledMainMessageContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  width: 100%;
+  height: 80%;
+  border: 1px solid white;
+`;
