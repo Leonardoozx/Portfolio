619cdb9e844f5ca3b4a6ad701661b5fa0df1bb27
Leonardoozx <leonardoguimaraesurci@gmail.com>
2023-02-24 19:57:07 -0300

    feat: implements MainMessage component

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,21 +1,9 @@
-import TypeWriterEffect from '@/components/TypeWriterEffect';
-import TypeWriter from 'typewriter-effect';
+import MainMessage from '@/components/MainMessage';
 
 export default function Home() {
   return (
     <div>
-      <TypeWriterEffect strings={"Hey, what's up?✌️"} />
-      <TypeWriter
-        onInit={(typewriter) => {
-          typewriter
-            .changeDelay(50)
-            .typeString('Welcome to my portfolio!')
-            .pauseFor(1000)
-            .deleteAll(10)
-            .typeString("I'm a FullStack Developer.")
-            .start();
-        }}
-      />
+     <MainMessage />
     </div>
   );
 }
